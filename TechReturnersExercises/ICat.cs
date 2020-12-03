8afd0845e4f9d929811f59b70f92f1712cd602e9
Dorota <d.a.lewinska@gmail.com>
2020-12-03 20:14:13 +0000

    Exercise completed

--- TechReturnersExercises/ICat.cs
+++ TechReturnersExercises/ICat.cs
@@ -4,6 +4,60 @@ namespace TechReturners.Exercises
 {
     public interface ICat
     {
+        bool IsAsleep { get; }
+        string Setting { get; }
+        float AverageHeight { get; }
+        string Eat { get; }
+        void GoToSleep();
+        void WakeUp();
 
+
+    }
+
+    public abstract class Cat
+    {
+        public bool IsAsleep { get; set; }
+        public string Setting { get; set; }
+        public string Eat { get; set; }
+        public float AverageHeight { get; set; }
+        public void GoToSleep()
+        {
+            IsAsleep = true;
+        }
+        public void WakeUp()
+        {
+            IsAsleep = false;
+        }
+    }
+
+    public class LionCat : Cat, ICat
+    {
+        public LionCat()
+        {
+            Eat = "Roar!!!!";
+            AverageHeight = 1100;
+            Setting = "Wild";
+
+        }
+    }
+    public class CheetahCat : Cat, ICat
+    {
+        public CheetahCat()
+        {
+            Eat = "Zzzzzzz";
+            AverageHeight = 300;
+            Setting = "Wild";
+
+        }
+    }
+    public class DomesticCat : Cat, ICat
+    {
+        public DomesticCat()
+        {
+            Eat = "Purrrrrrr";
+            AverageHeight = 23;
+            Setting = "domestic";
+
+        }
     }
 }
